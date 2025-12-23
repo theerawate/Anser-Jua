15c914bdb4ed472d973b2e515b261ed96a893c71
Jua <theerawate@gmail.com>
2025-12-23 23:03:51 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,22 @@
 Blocks.mechanicalDrill.drillTime=60
 Blocks.mechanicalDrill.tier=9
 Blocks.mechanicalDrill.itemCapacity=2000000
+Blocks.mechanicalDrill.liquidCapacity=2000000
 
 Blocks.pneumaticDrill.drillTime=40
 Blocks.pneumaticDrill.tier=9
 Blocks.pneumaticDrill.itemCapacity=2000000
+Blocks.pneumaticDrill.liquidCapacity=2000000
 
 Blocks.laserDrill.drillTime=28
 Blocks.laserDrill.tier=9
 Blocks.laserDrill.itemCapacity=2000000
+Blocks.laserDrill.liquidCapacity=2000000
 
 Blocks.blastDrill.drillTime=28
 Blocks.blastDrill.tier=9
 Blocks.blastDrill.itemCapacity=2000000
+Blocks.blastDrill.liquidCapacity=2000000
 
 Blocks.waterExtractor.pumpAmount=1.1
 Blocks.waterExtractor.liquidCapacity=2000000
@@ -34,15 +38,19 @@ Blocks.cliffCrusher.itemCapacity=2000000
 Blocks.plasmaBore.drillTime=16
 Blocks.plasmaBore.tier=9
 Blocks.plasmaBore.itemCapacity=2000000
+Blocks.plasmaBore.liquidCapacity=2000000
 
 Blocks.largePlasmaBore.drillTime=10
 Blocks.largePlasmaBore.tier=9
 Blocks.largePlasmaBore.itemCapacity=2000000
+Blocks.largePlasmaBore.liquidCapacity=2000000
 
 Blocks.impactDrill.drillTime=6
 Blocks.impactDrill.tier=9
 Blocks.impactDrill.itemCapacity=2000000
+Blocks.impactDrill.liquidCapacity=2000000
 
 Blocks.eruptionDrill.drillTime=6
 Blocks.eruptionDrill.tier=9
 Blocks.eruptionDrill.itemCapacity=2000000
+Blocks.eruptionDrill.liquidCapacity=2000000
