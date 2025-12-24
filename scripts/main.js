309545af434e2e1fdc59ec93e9039eae365e293d
Jua <theerawate@gmail.com>
2025-12-24 21:08:13 +0700

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,75 @@
 Blocks.mechanicalDrill.drillTime=60
 Blocks.mechanicalDrill.tier=9
-Blocks.mechanicalDrill.itemCapacity=2000000
-Blocks.mechanicalDrill.liquidCapacity=2000000
+Blocks.mechanicalDrill.itemCapacity=100000
+Blocks.mechanicalDrill.liquidCapacity=100000
 
 Blocks.pneumaticDrill.drillTime=40
 Blocks.pneumaticDrill.tier=9
-Blocks.pneumaticDrill.itemCapacity=2000000
-Blocks.pneumaticDrill.liquidCapacity=2000000
+Blocks.pneumaticDrill.itemCapacity=100000
+Blocks.pneumaticDrill.liquidCapacity=100000
 
 Blocks.laserDrill.drillTime=28
 Blocks.laserDrill.tier=9
-Blocks.laserDrill.itemCapacity=2000000
-Blocks.laserDrill.liquidCapacity=2000000
+Blocks.laserDrill.itemCapacity=100000
+Blocks.laserDrill.liquidCapacity=100000
 
 Blocks.blastDrill.drillTime=28
 Blocks.blastDrill.tier=9
-Blocks.blastDrill.itemCapacity=2000000
-Blocks.blastDrill.liquidCapacity=2000000
+Blocks.blastDrill.itemCapacity=100000
+Blocks.blastDrill.liquidCapacity=100000
 
 Blocks.waterExtractor.pumpAmount=1.1
-Blocks.waterExtractor.liquidCapacity=2000000
+Blocks.waterExtractor.liquidCapacity=100000
 
 Blocks.cultivator.craftTime=10
-Blocks.cultivator.itemCapacity=2000000
-Blocks.cultivator.liquidCapacity=2000000
+Blocks.cultivator.itemCapacity=100000
+Blocks.cultivator.liquidCapacity=100000
 
 Blocks.oilExtractor.pumpAmount=2.5
-Blocks.oilExtractor.itemCapacity=2000000
-Blocks.oilExtractor.liquidCapacity=2000000
+Blocks.oilExtractor.itemCapacity=100000
+Blocks.oilExtractor.liquidCapacity=100000
 
-Blocks.ventCondenser.craftTime = 12
-Blocks.ventCondenser.liquidCapacity=2000000
+Blocks.ventCondenser.craftTime=12
+Blocks.ventCondenser.liquidCapacity=100000
 
 Blocks.cliffCrusher.drillTime=11
-Blocks.cliffCrusher.itemCapacity=2000000
+Blocks.cliffCrusher.itemCapacity=100000
 
 Blocks.plasmaBore.drillTime=16
 Blocks.plasmaBore.tier=9
-Blocks.plasmaBore.itemCapacity=2000000
-Blocks.plasmaBore.liquidCapacity=2000000
+Blocks.plasmaBore.itemCapacity=100000
+Blocks.plasmaBore.liquidCapacity=100000
 
 Blocks.largePlasmaBore.drillTime=10
 Blocks.largePlasmaBore.tier=9
-Blocks.largePlasmaBore.itemCapacity=2000000
-Blocks.largePlasmaBore.liquidCapacity=2000000
+Blocks.largePlasmaBore.itemCapacity=100000
+Blocks.largePlasmaBore.liquidCapacity=100000
 
 Blocks.impactDrill.drillTime=6
 Blocks.impactDrill.tier=9
-Blocks.impactDrill.itemCapacity=2000000
-Blocks.impactDrill.liquidCapacity=2000000
+Blocks.impactDrill.itemCapacity=100000
+Blocks.impactDrill.liquidCapacity=100000
 
 Blocks.eruptionDrill.drillTime=6
 Blocks.eruptionDrill.tier=9
-Blocks.eruptionDrill.itemCapacity=2000000
-Blocks.eruptionDrill.liquidCapacity=2000000
+Blocks.eruptionDrill.itemCapacity=100000
+Blocks.eruptionDrill.liquidCapacity=100000
 
 Blocks.mechanicalPump.pumpAmount=7/6
-Blocks.mechanicalPump.liquidCapacity=2000000
+Blocks.mechanicalPump.liquidCapacity=100000
 
 Blocks.rotaryPump.pumpAmount=2
-Blocks.rotaryPump.liquidCapacity=2000000
+Blocks.rotaryPump.liquidCapacity=100000
 
 Blocks.impulsePump.pumpAmount=2.2
-Blocks.impulsePump.liquidCapacity=2000000
+Blocks.impulsePump.liquidCapacity=100000
 
-Blocks.turbineCondenser.liquidCapacity=2000000
-Blocks.chemicalCombustionChamber.liquidCapacity=2000000
-Blocks.pyrolysisGenerator.liquidCapacity=2000000
+Blocks.turbineCondenser.liquidCapacity=100000
+Blocks.chemicalCombustionChamber.liquidCapacity=100000
+Blocks.pyrolysisGenerator.liquidCapacity=100000
 
 Blocks.reinforcedPump.pumpAmount=800/60/4
-Blocks.reinforcedPump.liquidCapacity=2000000
+Blocks.reinforcedPump.liquidCapacity=100000
 
 UnitTypes.evoke.mineWalls=true
 UnitTypes.evoke.mineFloor=true
@@ -97,3 +97,6 @@ UnitTypes.emanate.targetable=true
 UnitTypes.emanate.hittable=true
 UnitTypes.emanate.envEnabled=1
 UnitTypes.emanate.engineSize=1
+
+Blocks.electrolyzer.liquidCapacity=100000
+Blocks.chemicalCombustionChamber.liquidCapacity=100000
